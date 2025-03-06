9595224be9eb9a6a9e2ce392c62582fce1665187
golda <goldaatemba610@gmail.com>
2025-03-06 09:44:16 +0300

    added files

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // code your solution here
+// Define a function using function declaration
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Define a function using function expression
+  const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Define a function that returns another function
+  function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Example usages
+  console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+  console.log(saturdayFun("hike")); // "This Saturday, I want to hike!"
+  
+  console.log(mondayWork()); // "This Monday, I will go to the office."
+  console.log(mondayWork("work from home")); // "This Monday, I will work from home."
+  
+  const encouragingPromptFunction = wrapAdjective("!!!");
+  console.log(encouragingPromptFunction("a hard worker")); // "You are !!!a hard worker!!!"
